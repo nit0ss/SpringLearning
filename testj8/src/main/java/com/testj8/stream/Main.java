7aa583d490c7b70e2d1a99607fc9f2c6e3c52db5
nit0ss <nitoss@mail.com>
2023-05-16 21:36:26 +0200

    Small changes
    Small changes

--- testj8/src/main/java/com/testj8/stream/Main.java
+++ testj8/src/main/java/com/testj8/stream/Main.java
@@ -1,6 +1,7 @@
 package com.testj8.stream;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -11,25 +12,46 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Main m = new Main();
 		m.cargarObjetos();
 		List<Curso> cursoLista = m.getCursosConMasDe10Videos();
 		List<Curso> cursoLista1 = m.getCursosConMasDe5h();
-		//cursoMuchosVideos.forEach(System.out::println);
-		/*
-		cursoMuchosVideos1.forEach(curso -> System.out.println("Duracion +5h =>" + curso));
-		System.out.println("---------------------------------");
-		cursoMuchosVideos1.forEach(curso -> System.out.println("Videos +10 =>" + curso));
-		//cursoMuchosVideos1.forEach(System.out::println);
-		System.out.println("---------------------------------");
-		*/
+		List<Curso> cursoLista2 = m.getCursosConMenos50VideosYMas300Alumnos();
+		// cursoMuchosVideos.forEach(System.out::println);
+
+		List<String> lista = Arrays.asList("Mitchum", "Delon", "Belmondo", "Affleck", "De Niro", "Al Pacino", "Daniel Craig", "Torrente", "Nuria Espert", "Wayne", "Darin", "Depardieu");
 		
-		IntStream.range(0, cursoLista1.size()).boxed().map(i -> "(Titulos +10) Encontrado!! : " + i  + cursoLista.get(i)).forEach(System.out::println);
-		System.out.println("---------------------------------");
-		IntStream.range(0, cursoLista.size()).boxed().map(i -> "(Duracion +5h) Encontrado!! : " + i  + cursoLista.get(i)).forEach(System.out::println);
+		System.out.println("-------- Empieza Por T ---------");
+		lista.stream().filter(actor -> actor.startsWith("T")).collect(Collectors.toList()).forEach(System.out::println);;
+		System.out.println("-------- Tiene U ---------");
+		lista.stream().filter(actor -> actor.contains("u")).collect(Collectors.toList()).forEach(System.out::println);
+
+		System.out.println("-------- Nombre compuesto ---------");
+		List<String> actoresFiltrados = lista.stream().filter(actor -> {
+                    String[] nombres = actor.split(" ");
+                    return nombres.length > 1;
+                })
+                .sorted().collect(Collectors.toList());
+		
+		actoresFiltrados.forEach(System.out::println);
+
 
 		
+		
+		// cursoMuchosVideos1.forEach(curso -> System.out.println("Duracion +5h =>" +
+		// curso));
+		System.out.println("---------------------------------");
+		cursoLista2.forEach(curso -> System.out.println("Filtrado => " + curso));
+		// cursoMuchosVideos1.forEach(System.out::println);
+		System.out.println("---------------------------------");
+
+		IntStream.range(0, cursoLista1.size()).boxed()
+				.map(i -> "(Titulos +10) Encontrado!!: " + i + "-" + cursoLista.get(i)).forEach(System.out::println);
+		System.out.println("---------------------------------");
+		IntStream.range(0, cursoLista.size()).boxed()
+				.map(i -> "(Duracion +5h) Encontrado!! : " + i + "-" + cursoLista.get(i)).forEach(System.out::println);
+
 	}
 
 	public void cargarObjetos() {
@@ -42,17 +64,24 @@ public class Main {
 	}
 
 	
-	public  List<Curso> getCursosConMasDe10Videos(){
-		
-		return  cursos.stream().filter(c -> c.getVideos() > 10).collect(Collectors.toList()); 
-		
+	
+	
+	public List<Curso> getCursosConMasDe10Videos() {
+
+		return cursos.stream().filter(c -> c.getVideos() > 10).collect(Collectors.toList());
+
 	}
 
-	public  List<Curso> getCursosConMasDe5h(){
-		
-		return  cursos.stream().filter(c -> c.getDuracion() > 5).collect(Collectors.toList()); 
-		
+	public List<Curso> getCursosConMasDe5h() {
+
+		return cursos.stream().filter(c -> c.getDuracion() > 5).collect(Collectors.toList());
+
 	}
-	
-	
+
+	public List<Curso> getCursosConMenos50VideosYMas300Alumnos() {
+
+		return cursos.stream().filter(c -> c.getVideos() < 50 && c.getAlumnos() > 300).collect(Collectors.toList());
+
+	}
+
 }
